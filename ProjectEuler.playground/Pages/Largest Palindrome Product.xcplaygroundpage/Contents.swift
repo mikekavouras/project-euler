99fd9fe9bac5196c05cb0f8588fe685178dff293
mikekavouras <kavourasm@gmail.com>
2015-11-07 10:09:52 -0500

    problem number 5

---
+++ ProjectEuler.playground/Pages/Largest Palindrome Product.xcplaygroundpage/Contents.swift
@@ -0,0 +1,86 @@
+//: [Even Fibonacci numbers](@previous)
+
+/*:
+# Largest Palindrome Product
+https://projecteuler.net/problem=4
+
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 * 99.
+
+**Find the largest palindrome made from the product of two 3-digit numbers**
+*/
+
+import Foundation
+
+//: First attempt
+
+/*
+
+extension Int {
+    func isPalindrome() -> Bool {
+        let str = String(format: "%d", arguments: [self])
+        return str == str.reverse()
+    }
+}
+
+extension String {
+    func reverse() -> String {
+        var r = [Character]()
+        for (_, char) in self.characters.enumerate() {
+            r.insert(char, atIndex: 0)
+        }
+        return String(r)
+    }
+}
+
+func largestPalindromWithXNumberOfDigits(numberOfDigits: Int) -> Int {
+    let largest = Int((0..<numberOfDigits).map{_ in "9"}.joinWithSeparator(""))!
+    var results = [Int]()
+    for var i = largest; i >= 0; i-- {
+        for var j = largest; j >= 0; j-- {
+            if (i * j).isPalindrome() {
+                results.append(i * j)
+            }
+        }
+    }
+    return results.sort { $0 > $1 }.first!
+}
+
+print(NSDate())
+print(largestPalindromWithXNumberOfDigits(3))
+print(NSDate())
+
+*/
+
+//: Second attempt
+
+extension Int {
+    func isPalindrome() -> Bool {
+        var reversedNum = 0
+        var digit = 0
+        var remainingNum = self
+        while(remainingNum > 0) {
+            digit = remainingNum % 10
+            reversedNum += digit
+            reversedNum *= 10
+            remainingNum = (remainingNum - digit) / 10
+        }
+        return self == (reversedNum / 10)
+    }
+}
+
+func largestPalindromWithXNumberOfDigits(numberOfDigits: Int) -> Int {
+    let largest = Int((0..<numberOfDigits).map{_ in "9"}.joinWithSeparator(""))!
+    var results = [Int]()
+    for var i = largest; i >= 10; i-- {
+        for var j = largest; j >= 10; j-- {
+            if (i * j).isPalindrome() {
+                results.append(i * j)
+            }
+        }
+    }
+    return results.sort { $0 > $1 }.first!
+}
+
+//print(largestPalindromWithXNumberOfDigits(3))
+
+//: [Next](@next)
---
+++ ProjectEuler.playground/Pages/Smallest Multiple.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+//: [Largest Palindrome Product](@previous)
+
+/*:
+# Smallest Multiple
+https://projecteuler.net/problem=5
+
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+**What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20**
+*/
+
+import Foundation
+
+//: First attempt
+
+func smallestMultiple(range: Range<Int>) -> Int {
+    var result: Int = -1
+    for var i = 1;;i++ {
+        var found = 0
+        for j in range {
+            guard i % j == 0 else { break }
+            found++
+            result = i
+        }
+        if found == range.count {
+            return result
+        }
+    }
+}
+
+//smallestMultiple(1...20)
+
+//: [Next](@next)
